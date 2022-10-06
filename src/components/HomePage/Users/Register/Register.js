f962d8b21ca58ab32ffc1b244f1f0da92d05ea38
Ramya Mol R <ramya45r@gmail.com>
2022-10-06 23:12:33 +0530

    signup completed

--- src/components/HomePage/Users/Register/Register.js
+++ src/components/HomePage/Users/Register/Register.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { useFormik } from "formik"
 import * as Yup from "yup"
+import { useDispatch, useSelector } from "react-redux";
+import { registerUserAction } from "../../../../redux/slices/users/usersSlices";
 
 //Form schema
 const formSchema = Yup.object({
@@ -13,6 +15,8 @@ const formSchema = Yup.object({
 //Register
 //-------------------------------
 const Register = () => {
+  //dispatch
+  const dispatch =useDispatch()
   //formik
   const formik = useFormik({
     initialValues: {
@@ -22,11 +26,16 @@ const Register = () => {
       password: "",
     },
     onSubmit: values => {
-      console.log(values);
+     //dispatch action
+     dispatch(registerUserAction(values))
+     console.log(values);
     },
     validationSchema: formSchema,
   });
-
+//select state from store
+   const storeData = useSelector(store=>store?.users);
+   const {loading,appErr,serverErr,registered} =storeData;
+   console.log(appErr,serverErr);
   return (
     <section className="relative py-20 2xl:py-40 bg-gray-800 overflow-hidden">
       <div className="relative container px-4 mx-auto">
@@ -46,8 +55,15 @@ const Register = () => {
               <div className="px-6 lg:px-20 py-12 lg:py-24 bg-gray-600 rounded-lg">
                 <form onSubmit={formik.handleSubmit}>
                   <h3 className="mb-10 text-2xl text-white font-bold font-heading">
-                    Register Account–
+                    Register Account
+                    {/* display error message*/}
+                    {appErr || serverErr ? (
+                      <div className="text-red-400">
+                        {serverErr} {appErr}
+                      </div>
+                    ) : null}
                   </h3>
+
                   {/* First name */}
                   <div className="flex items-center pl-6 mb-3 bg-white rounded-full">
                     <span className="inline-block pr-3 py-2 border-r border-gray-50">
@@ -243,12 +259,22 @@ const Register = () => {
 
                   <div className="inline-flex mb-10"></div>
 
-                  <button
-                    type="submit"
-                    className="py-4 w-full bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-full transition duration-200"
-                  >
-                    Register
-                  </button>
+                  {/* Check for loading */}
+                  {loading ? (
+                    <button
+                      disabled
+                      className="py-4 w-full bg-gray-500  text-white font-bold rounded-full transition duration-200"
+                    >
+                      loading please wait...
+                    </button>
+                  ) : (
+                    <button
+                      type="submit"
+                      className="py-4 w-full bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-full transition duration-200"
+                    >
+                      Register
+                    </button>
+                  )}
                 </form>
               </div>
             </div>
